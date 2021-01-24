095c583d7cc791d7bacab528100faac138c303f5
Andrei Calazans <andreixoc@hotmail.com>
2021-01-24 08:40:38 -0300

    Add test for auto height usage

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   StyleSheet,
   Text,
@@ -7,7 +7,7 @@ import {
   Button,
   SafeAreaView,
   ScrollView,
-} from 'react-native';
+} from "react-native";
 /* import { */
 /*   createBottomTabNavigator, */
 /*   createStackNavigator, */
@@ -15,11 +15,11 @@ import {
 /*   HeaderMode, */
 /* } from 'react-navigation'; */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Tooltip from 'rn-tooltip';
+import Tooltip from "rn-tooltip";
 
 const BasicExample = () => {
   return (
@@ -41,8 +41,8 @@ const RowOfTooltips = () => {
   );
 };
 
-const MakeScreen = name => () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+const MakeScreen = (name) => () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text>{name}</Text>
   </View>
 );
@@ -67,8 +67,8 @@ const Tab = createBottomTabNavigator();
 function Tabs() {
   return (
     <Tab.Navigator backBehavior="none">
-      <Tab.Screen name="FirstScreen" component={MakeScreen('First')} />
-      <Tab.Screen name="SecondScreen" component={MakeScreen('Second')} />
+      <Tab.Screen name="FirstScreen" component={MakeScreen("First")} />
+      <Tab.Screen name="SecondScreen" component={MakeScreen("Second")} />
     </Tab.Navigator>
   );
 }
@@ -77,21 +77,24 @@ const InsideScrollExample = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.allOverContainer}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={[styles.row, { alignItems: 'stretch', marginTop: 50 }]}>
-          <Tooltip
-            highlightColor="blue"
-            height={250}
-            withOverlay
-            popover={
-              <Text>
-                Note that the tooltip wraps the parent View. This is because it takes the
-                measurement from the parent. Also it's not smart enought to grow based on the size
-                of the text, you have to imperatively add width and height
-              </Text>
-            }
-          >
-            <Text>Big Text</Text>
-          </Tooltip>
+        <View style={[styles.row, { alignItems: "stretch", marginTop: 50 }]}>
+          <View>
+            <Tooltip
+              highlightColor="blue"
+              height={250}
+              withOverlay
+              popover={
+                <Text>
+                  Note that the tooltip wraps the parent View. This is because
+                  it takes the measurement from the parent. Also it's not smart
+                  enought to grow based on the size of the text, you have to
+                  imperatively add width and height
+                </Text>
+              }
+            >
+              <Text>Big Text</Text>
+            </Tooltip>
+          </View>
           <View>
             <Tooltip highlightColor="yellow" popover={<Text>Info Two</Text>}>
               <Text>Smaller parent</Text>
@@ -103,8 +106,12 @@ const InsideScrollExample = ({ navigation }) => {
             </Tooltip>
           </View>
         </View>
-        <View style={{ width: 100, backgroundColor: 'red' }}>
-          <Tooltip overlayColor="lightpink" withOverlay popover={<Text>Info Three</Text>}>
+        <View style={{ width: 100, backgroundColor: "red" }}>
+          <Tooltip
+            overlayColor="lightpink"
+            withOverlay
+            popover={<Text>Info Three</Text>}
+          >
             <Text>With overlay</Text>
           </Tooltip>
         </View>
@@ -126,7 +133,7 @@ const InsideScrollExample = ({ navigation }) => {
 const AllOver = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.allOverContainer}>
-      <View style={{ height: 100, justifyContent: 'center', width: 100 }}>
+      <View style={{ height: 100, justifyContent: "center", width: 100 }}>
         <Tooltip
           width="80%"
           highlightColor="yellow"
@@ -135,21 +142,24 @@ const AllOver = ({ navigation }) => {
           <Text>Width as percent</Text>
         </Tooltip>
       </View>
-      <View style={[styles.row, { alignItems: 'stretch' }]}>
-        <Tooltip
-          highlightColor="blue"
-          height={250}
-          withOverlay
-          popover={
-            <Text>
-              Note that the tooltip wraps the parent View. This is because it takes the measurement
-              from the parent. Also it's not smart enought to grow based on the size of the text,
-              you have to imperatively add width and height
-            </Text>
-          }
-        >
-          <Text>Big Text</Text>
-        </Tooltip>
+      <View style={[styles.row, { alignItems: "stretch" }]}>
+        <View>
+          <Tooltip
+            highlightColor="blue"
+            height={250}
+            withOverlay
+            popover={
+              <Text>
+                Note that the tooltip wraps the parent View. This is because it
+                takes the measurement from the parent. Also it's not smart
+                enought to grow based on the size of the text, you have to
+                imperatively add width and height
+              </Text>
+            }
+          >
+            <Text>Big Text</Text>
+          </Tooltip>
+        </View>
         <View>
           <Tooltip highlightColor="yellow" popover={<Text>Info Two</Text>}>
             <Text>Smaller parent</Text>
@@ -184,29 +194,147 @@ const AllOver = ({ navigation }) => {
               <ScrollView style={{ width: 300, height: 200 }}>
                 <View onStartShouldSetResponder={() => true}>
                   <Text>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia
-                    tenetur tempora quibusdam cupiditate non architecto deserunt amet, ipsam fuga
-                    quae maxime suscipit dignissimos, dicta minus sit commodi eius quod.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
                   </Text>
                   <Text>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia
-                    tenetur tempora quibusdam cupiditate non architecto deserunt amet, ipsam fuga
-                    quae maxime suscipit dignissimos, dicta minus sit commodi eius quod.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
                   </Text>
                   <Text>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia
-                    tenetur tempora quibusdam cupiditate non architecto deserunt amet, ipsam fuga
-                    quae maxime suscipit dignissimos, dicta minus sit commodi eius quod.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
                   </Text>
                   <Text>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia
-                    tenetur tempora quibusdam cupiditate non architecto deserunt amet, ipsam fuga
-                    quae maxime suscipit dignissimos, dicta minus sit commodi eius quod.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
                   </Text>
                   <Text>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia
-                    tenetur tempora quibusdam cupiditate non architecto deserunt amet, ipsam fuga
-                    quae maxime suscipit dignissimos, dicta minus sit commodi eius quod.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
+                  </Text>
+                </View>
+              </ScrollView>
+            }
+          >
+            <Text>Large text with scroll</Text>
+          </Tooltip>
+        </View>
+      </View>
+      <RowOfTooltips />
+      <Button title="Go Back" onPress={() => navigation.goBack(null)} />
+    </SafeAreaView>
+  );
+};
+
+const WithAutoHeight = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.allOverContainer}>
+      <View style={{ height: 100, justifyContent: "center", width: 100 }}>
+        <Tooltip
+          width="80%"
+          height="auto"
+          highlightColor="yellow"
+          popover={<Text>Width as percent was broken before</Text>}
+        >
+          <Text>Width as percent</Text>
+        </Tooltip>
+      </View>
+      <View style={[styles.row, { alignItems: "stretch" }]}>
+        <View>
+          <Tooltip
+            highlightColor="blue"
+            height="auto"
+            withOverlay
+            popover={
+              <Text>
+                Note that the tooltip wraps the parent View. This is because it
+                takes the measurement from the parent. Also it's not smart
+                enought to grow based on the size of the text, you have to
+                imperatively add width and height
+              </Text>
+            }
+          >
+            <Text>Big Text</Text>
+          </Tooltip>
+        </View>
+        <View>
+          <Tooltip
+            height="auto"
+            highlightColor="yellow"
+            popover={<Text>Info Two</Text>}
+          >
+            <Text>Smaller parent</Text>
+          </Tooltip>
+        </View>
+        <BasicExample />
+      </View>
+      <View style={styles.row}>
+        <View>
+          <Tooltip
+            height="auto"
+            toggleWrapperProps={{
+              hitSlop: {
+                top: 40,
+                left: 20,
+                bottom: 40,
+                right: 20,
+              },
+            }}
+            overlayColor="lightpink"
+            withOverlay
+            popover={<Text>Info Three</Text>}
+          >
+            <Text>Improve hit slop example</Text>
+          </Tooltip>
+        </View>
+
+        <View>
+          <Tooltip
+            height="auto"
+            width={300}
+            popover={
+              <ScrollView style={{ width: 300, height: 200 }}>
+                <View onStartShouldSetResponder={() => true}>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur quia tenetur tempora quibusdam cupiditate non
+                    architecto deserunt amet, ipsam fuga quae maxime suscipit
+                    dignissimos, dicta minus sit commodi eius quod.
                   </Text>
                 </View>
               </ScrollView>
@@ -225,7 +353,7 @@ const AllOver = ({ navigation }) => {
 const Showcase = ({ title, description, onPress }) => {
   return (
     <View style={styles.showcaseBox}>
-      <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
+      <TouchableOpacity activeOpacity={0.6} onPress={onPress} style={{ width: '100%' }}>
         <React.Fragment>
           <Text style={styles.showcaseTitle}>{title}</Text>
           <Text style={styles.showcaseDescription}>{description}</Text>
@@ -240,19 +368,24 @@ const HomeScreen = ({ navigation }) => {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>React Native Tooltip Showcase App</Text>
       <Showcase
-        onPress={() => navigation.navigate('InScroll')}
+        onPress={() => navigation.navigate("InScroll")}
         title="Inside ScrollView"
         description="Example displaying how the tooltip behaves inside a scrollview."
       />
       <Showcase
-        onPress={() => navigation.navigate('Tabs')}
+        onPress={() => navigation.navigate("Tabs")}
         title="With Tabs"
         description="rn-tooltip usage inside tab screens, in the bottom tab and header."
       />
       <Showcase
-        onPress={() => navigation.navigate('AllOver')}
+        onPress={() => navigation.navigate("AllOver")}
         title="All Directions"
         description="In this example we render multiple tooltips in different places to display how it behaves"
+      />
+      <Showcase
+        onPress={() => navigation.navigate("WithAutoHeight")}
+        title="With Auto Height"
+        description="Test the usage of auto height"
       />
     </ScrollView>
   );
@@ -268,6 +401,7 @@ function App() {
         <Stack.Screen name="InScroll" component={InsideScrollExample} />
         <Stack.Screen name="Tabs" component={Tabs} />
         <Stack.Screen name="AllOver" component={AllOver} />
+        <Stack.Screen name="WithAutoHeight" component={WithAutoHeight} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -278,28 +412,28 @@ export default App;
 const styles = StyleSheet.create({
   header: {
     padding: 30,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
     height: 90,
-    backgroundColor: 'yellow',
+    backgroundColor: "yellow",
   },
   allOverContainer: {
     margin: 20,
     flexGrow: 1,
   },
   row: {
-    alignItems: 'center',
+    alignItems: "center",
     height: 150,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   showcaseBox: {
-    alignItems: 'center',
-    margin: 30,
+    alignItems: "center",
     padding: 20,
-    shadowColor: '#000',
+    width: "100%",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 10,
@@ -313,19 +447,18 @@ const styles = StyleSheet.create({
   },
   showcaseDescription: {
     fontSize: 14,
-    color: '#333',
+    color: "#333",
   },
   title: {
     fontSize: 28,
   },
   container: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   goBack: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
